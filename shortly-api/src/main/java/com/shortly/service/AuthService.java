dd11e3ac41eab70e336e940269adddda3367e9b4
Dayna Yoon <dawon020411@gmail.com>
2026-03-16 22:57:30 -0600

    docs: add method stubs and descriptions to service classes

---
+++ shortly-api/src/main/java/com/shortly/service/AuthService.java
@@ -0,0 +1,6 @@
+package com.shortly.service;
+
+public class AuthService {
+    // register(AuthRequest request) -> AuthResponse
+    // login(AuthRequest request) -> AuthResponse
+}
--- shortly-api/src/main/java/com/shortly/service/UrlService.java
+++ shortly-api/src/main/java/com/shortly/service/UrlService.java
@@ -1,5 +1,8 @@
 package com.shortly.service;
 
 public class UrlService {
-    
+    // shorten(ShortenRequest, User) -> UrlResponse <- shorten a URL
+    // getUserUrls(User) -> List<UrlResponse> <- get all URLs created by a user
+    // delete(id, User) -> void <- delete a URL if it belongs to the user
+    // toggleActive(id, User) -> void <- toggle the active status of a URL if it belongs to the user
 }
