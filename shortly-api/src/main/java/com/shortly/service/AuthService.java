7c5a03c19672a6a28854bd531c9d67b15aa90a09
Dayna Yoon <dawon020411@gmail.com>
2026-04-19 02:06:49 -0600

    feat: implement AnalyticsService.getStats()

--- shortly-api/src/main/java/com/shortly/service/AuthService.java
+++ shortly-api/src/main/java/com/shortly/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.shortly.service;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import com.shortly.dto.AuthResponse;
 import com.shortly.dto.GoogleAuthRequest;
 import com.shortly.model.User;
@@ -7,7 +11,11 @@ import com.shortly.repository.UserRepository;
 import com.shortly.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -30,7 +38,38 @@ public class AuthService {
      * 5. Return AuthResponse
      */
     public AuthResponse googleLogin(GoogleAuthRequest request) {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not yet implemented");
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
+                new NetHttpTransport(), new GsonFactory())
+                .setAudience(Collections.singletonList(googleClientId))
+                .build();
+
+        GoogleIdToken idToken;
+        try {
+            idToken = verifier.verify(request.getCredential());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Google token");
+        }
+
+        if (idToken == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Google token");
+        }
+
+        GoogleIdToken.Payload payload = idToken.getPayload();
+        String googleId = payload.getSubject();
+        String email = payload.getEmail();
+        String name = (String) payload.get("name");
+        String picture = (String) payload.get("picture");
+
+        User user = userRepository.findByGoogleId(googleId).orElseGet(() -> {
+            User newUser = new User();
+            newUser.setGoogleId(googleId);
+            newUser.setEmail(email);
+            newUser.setName(name);
+            newUser.setProfilePicture(picture);
+            return userRepository.save(newUser);
+        });
+
+        String jwt = jwtUtil.generateToken(user.getEmail());
+        return new AuthResponse(jwt, user.getEmail(), user.getName(), user.getProfilePicture());
     }
 }
